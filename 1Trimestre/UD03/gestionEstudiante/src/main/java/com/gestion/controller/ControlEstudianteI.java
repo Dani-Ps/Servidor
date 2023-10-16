dc429cfd347c80036c0c8f6ed36b501029b1215e
Daniel Perez Serrano <daniperez439@gmail.com>
2023-10-16 18:47:58 +0200

    Index Working

--- 1Trimestre/UD03/gestionEstudiante/src/main/java/com/gestion/controller/ControlEstudianteI.java
+++ 1Trimestre/UD03/gestionEstudiante/src/main/java/com/gestion/controller/ControlEstudianteI.java
@@ -3,15 +3,17 @@ package com.gestion.controller;
 import java.util.List;
 
 import com.gestion.model.Alumno;
+import com.gestion.model.Alumno.Curso;
 
 public interface ControlEstudianteI {
 	
 	public boolean listar();
-	public boolean añadir(Alumno a);
-	public Alumno leer(String nombre);
+	public String añadir(String nombre, Integer edad, Curso curso );
+	public boolean leer(Alumno a);
 	public List<Alumno> getAll();
 	public boolean eliminar(String nombre);
 	public List<Alumno> filtrar();
+	boolean existe(Alumno a);
 	
 	
 	
--- 1Trimestre/UD03/gestionEstudiante/src/main/java/com/gestion/controller/ControlEstudianteImp.java
+++ 1Trimestre/UD03/gestionEstudiante/src/main/java/com/gestion/controller/ControlEstudianteImp.java
@@ -3,11 +3,17 @@ package com.gestion.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.gestion.model.Alumno;
+import com.gestion.model.Alumno.Curso;
 
-public class ControlEstudianteImp implements ControlEstudianteI{
+@Controller
+public class ControlEstudianteImp implements ControlEstudianteI {
 
-	private List<Alumno> alumnos = new ArrayList<Alumno>(); 
+	private List<Alumno> alumnos = new ArrayList<Alumno>();
 
 	@Override
 	public boolean listar() {
@@ -15,14 +21,8 @@ public class ControlEstudianteImp implements ControlEstudianteI{
 	}
 
 	@Override
-	public boolean añadir(Alumno a) {
-		
-		return false;
-	}
-
-	@Override
-	public Alumno leer(String nombre) {
-		return null;
+	public boolean existe(Alumno a) {
+		return alumnos.stream().anyMatch(alumno -> alumno.equals(a));
 	}
 
 	@Override
@@ -42,4 +42,30 @@ public class ControlEstudianteImp implements ControlEstudianteI{
 		return null;
 	}
 
+	@PostMapping("/crear")
+	@Override
+	public String añadir(String nombre, Integer edad, Curso curso) {
+		try {
+			Alumno a = new Alumno(nombre, edad, curso);
+			if (existe(a)) {
+				mostrarExistError();
+			} else {
+				alumnos.add(a);
+			}
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return "exito";
+	}
+
+	@Override
+	public boolean leer(Alumno a) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@GetMapping("/mostrarExistError")
+	public String mostrarExistError() {
+		return "error/exist";
+	}
 }
